26df17431b307eb26e56df7a684277d7e43d5e2a
nbendall34 <33398449+nbendall34@users.noreply.github.com>
2020-01-05 17:03:25 +0000

    Hosting Added

---
+++ wpscripts/global_navtree.js
@@ -0,0 +1,28 @@
+var global_navtree = WpNavBar.readTree({
+"childArray" : [
+{   'sTitle':'Home',
+'bIsWebPath':true,
+'sUrl':'index.html',
+'sTarget':'_self'
+},
+{   'sTitle':'Contact',
+'bIsWebPath':true,
+'sUrl':'contact.html',
+'sTarget':'_self'
+},
+{   'sTitle':'New Zealand',
+'bIsWebPath':true,
+'sUrl':'new_zealand.html',
+'sTarget':'_self'
+},
+{   'sTitle':'Iceland',
+'bIsWebPath':true,
+'sUrl':'iceland.html',
+'sTarget':'_self'
+},
+{   'sTitle':'Japan',
+'bIsWebPath':true,
+'sUrl':'japan.html',
+'sTarget':'_self'
+}]
+});
